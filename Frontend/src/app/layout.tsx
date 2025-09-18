b2aed3d8c819af626f1b423bc0cadd31f168507b
DhruvPopat268 <dhruvpopat268@gmail.com>
2025-09-18 19:23:13 +0530

    updated code

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
-import { AuthProvider } from '@/context/AuthContext';
 
 const outfit = Outfit({
   subsets: ["latin"],
